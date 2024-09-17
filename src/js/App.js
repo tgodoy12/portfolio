f88e65101f8400f93c832923a3242e0c58347946
Trilce Godoy <trilcegodoy11@gmail.com>
2024-09-16 23:37:55 -0300

    styling landing page

--- src/js/App.js
+++ src/js/App.js
@@ -3,11 +3,13 @@ import Landing from './pages/landing';
 import Contact from './pages/contact';
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from './components/navbar';
 
 function App() {
   return (
    
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/contact" element={<Contact />}/>
---
+++ src/js/components/navbar.js
@@ -0,0 +1,95 @@
+import React, {useState} from "react";
+import { Dialog, DialogPanel } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import trilce from "./../img/trilce.svg"
+
+const Navbar = () => {
+
+    const navigation = [
+        { name: 'About', href: '#' },
+        { name: 'Skills', href: '#' },
+        { name: 'Projects', href: '#' },
+        { name: 'Contact', href: '#' },
+    ]
+
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+    return (
+        <div>
+            <header className="absolute inset-x-0 top-0 z-50">
+        <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
+          <div className="flex lg:flex-1">
+            <a href="/" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img
+                alt="trilce icon"
+                src={trilce}
+                style={{ height: '60px', width: '60px' }}
+                className="object-cover align-middle"
+              />
+            </a>
+          </div>
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(true)}
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+            </button>
+          </div>
+          <div className="hidden lg:flex lg:gap-x-12 items-baseline">
+            {navigation.map((item) => (
+              <a key={item.name} href={item.href} className="navbarlist text-sm font-semibold leading-6 text-gray-900">
+                {item.name}
+              </a>
+            ))}
+          </div>
+        </nav>
+        <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+          <div className="fixed inset-0 z-50" />
+          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-[#000000] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+              <a href="/" className="-m-1.5 p-1.5">
+                <span className="sr-only">Your Company</span>
+                <img
+                alt="trilce icon"
+                src={trilce}
+                style={{ height: '60px', width: '60px' }}
+                className="object-cover align-middle"
+                />
+              </a>
+              <button
+                type="button"
+                onClick={() => setMobileMenuOpen(false)}
+                className="-m-2.5 rounded-md p-2.5 text-[#7F95D1]"
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon aria-hidden="true" className="h-6 w-6" />
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="-mx-3 navbarlist block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-[#FF6978]"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </DialogPanel>
+        </Dialog>
+      </header>
+    </div>
+
+    )
+}
+
+export default Navbar;
