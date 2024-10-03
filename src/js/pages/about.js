30c1d8d3ee526e22de59257ba0e5c6474eb50371
Trilce Godoy <trilcegodoy11@gmail.com>
2024-10-03 00:51:27 -0300

    about page taking shape

--- src/js/pages/about.js
+++ src/js/pages/about.js
@@ -4,26 +4,27 @@ const About = () => {
 
     // agregar: download cv, certificates in grid, avatar and carousel skills
     return (
-        <div className="min-h-screen bg-[#7F95D1] flex items-center justify-center">
+        <div className="min-h-screen bg-[#7F95D1] flex items-center">
 
-            <div className="column-2">
-                <div className="about-text mx-4">
+            <div className="column-2 w-1/2">
+                <div className="avatar-box flex justify-center">
+                    <img 
+                    src="https://picsum.photos/300" 
+                    alt="avatar" 
+                    className="rounded-full"/>
+                </div>
+
+            </div>
+
+            <div className="column-2 w-1/2">
+                
+                <div className="about-text flex justify-center me-12">
                     <p>I'm a passionate Fullstack Developer with a love for creating effecive web solutions .
                         My journey into tech started with a deep curiosity about how websites and applications are built, which led me to immerse myself in learning everything from frontend design to backend functionality.
                     </p>
                 </div>
 
-            </div>
-
-            <div className="column-2">
-                <div className="column-2">
-                    <div className="about-text mx-4">
-                        <p>I'm a passionate Fullstack Developer with a love for creating effecive web solutions .
-                            My journey into tech started with a deep curiosity about how websites and applications are built, which led me to immerse myself in learning everything from frontend design to backend functionality.
-                        </p>
-                    </div>
-
-                </div>
+               
             </div>
             
         </div>
