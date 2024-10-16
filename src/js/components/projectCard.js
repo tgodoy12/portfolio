e9af03f88464a5ec95305c00c585718b96de6da0
Trilce Godoy <trilcegodoy11@gmail.com>
2024-10-16 00:21:02 -0300

    grid responsive, json for projects created

--- src/js/components/projectCard.js
+++ src/js/components/projectCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ProjectCard = () => {
     return (
-        <div className="border border-amber-50 h-40 sm:w-full min-w-40">
+        <div className="border border-amber-50 h-48 w-full min-w-40">
             
         </div>
     )
--- src/js/components/projects.js
+++ src/js/components/projects.js
@@ -6,11 +6,15 @@ import ProjectCard from "./projectCard";
 const Projects = () => {
 
     return (
-        <div className=" min-h-screen bg-[#000000] mx-auto px-6 sm:px-10 lg:px-24 xl:px-36">
-            {/* <PolygonTop/> */}
+        <div className=" min-h-screen bg-[#000000] flex flex-col justify-center mx-auto px-24 sm:px-32 lg:px-36 xl:px-72">
+            
+            <div className="title text-3xl mb-24">
+                Projects
+            </div>
 
             <div class="
                 grid grid-cols-1 gap-4
+                justify-items-center
                 md:grid-cols-2 
                 xl:grid-cols-3">
                 <ProjectCard />
